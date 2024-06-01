eb904d7147c1993946816629f7a6580d183ff378
Joel Viloria <viloriajoel07@github.com>
2024-06-01 09:13:20 -0500

    add titles to creation menu

--- src/modules/layouts/templates/dashbaord/Header/CreationMenu/CreationMenu.tsx
+++ src/modules/layouts/templates/dashbaord/Header/CreationMenu/CreationMenu.tsx
@@ -5,10 +5,20 @@ import { useParams } from "next/navigation";
 import { useOutsideClick } from "~/lib/hooks";
 
 import { Icon } from "@iconify/react/dist/iconify.js";
-import { Button, Listbox, ListboxItem } from "@nextui-org/react";
+import {
+  Button,
+  Listbox,
+  ListboxItem,
+  ListboxSection,
+} from "@nextui-org/react";
 
 import type { ListMenu } from "~/types/root.types";
 import { useResize } from "~/lib/hooks/useResize";
+
+interface OptionsProps {
+  title: string;
+  menus: ListMenu[];
+}
 
 const CreationMenu = () => {
   const router = useRouter();
@@ -21,38 +31,46 @@ const CreationMenu = () => {
 
   const BASIC_URL = `/account/${acc}/`;
 
-  const OPTIONS: ListMenu[] = [
+  const OPTIONS: OptionsProps[] = [
     {
-      label: "Ingreso",
-      icon: "iconamoon:arrow-top-right-1-light",
-      href: `${BASIC_URL}transactions/new`,
+      title: "Transacciones",
+      menus: [
+        {
+          label: "Ingreso",
+          icon: "iconamoon:arrow-top-right-1-light",
+          href: `${BASIC_URL}transactions/new`,
+        },
+        {
+          label: "Egreso",
+          icon: "iconamoon:arrow-bottom-left-1-light",
+          href: `${BASIC_URL}transactions/new?type=2`,
+        },
+        {
+          label: "Meta",
+          icon: "mdi:cash-minus",
+          href: `${BASIC_URL}transactions/new?type=2`,
+        },
+      ],
     },
     {
-      label: "Egreso",
-      icon: "iconamoon:arrow-bottom-left-1-light",
-      href: `${BASIC_URL}transactions/new?type=2`,
-    },
-    {
-      label: "Meta",
-      icon: "mdi:cash-minus",
-      showLine: true,
-      href: `${BASIC_URL}transactions/new?type=2`,
-    },
-    {
-      label: "Nueva Meta",
-      showLine: true,
-      icon: "ph:target",
-      href: `${BASIC_URL}transactions/new?type=2`,
-    },
-    {
-      label: "Categoria",
-      icon: "iconamoon:category",
-      href: `${BASIC_URL}category/new`,
-    },
-    {
-      label: "Entidad",
-      icon: "iconamoon:category",
-      href: `${BASIC_URL}category/new`,
+      title: "Servicios",
+      menus: [
+        {
+          label: "Nueva Meta",
+          icon: "ph:target",
+          href: `${BASIC_URL}transactions/new?type=2`,
+        },
+        {
+          label: "Categoria",
+          icon: "iconamoon:category",
+          href: `${BASIC_URL}category/new`,
+        },
+        {
+          label: "Entidad",
+          icon: "ph:users-bold",
+          href: `${BASIC_URL}entities/new`,
+        },
+      ],
     },
   ];
 
@@ -77,20 +95,30 @@ const CreationMenu = () => {
         {isDesktop && "Crear"}
       </Button>
       {showMenu && (
-        <div className="absolute right-0 top-12 w-[165px] rounded-small border-small border-default-200 bg-white/80 backdrop-blur-md  dark:border-default-100 dark:border-white/5 dark:bg-slate-900/80">
-          <Listbox variant="flat" aria-label="Creation menu">
-            {OPTIONS.map(({ href, label, showLine = false, icon }, index) => {
+        <div className="absolute right-0 top-12 w-[165px] rounded-small border-small border-default-200 backdrop-blur-xl  dark:border-default-100 dark:border-white/5">
+          <Listbox
+            variant="flat"
+            aria-label="Creation menu"
+            className="bg-opacity-65 backdrop-blur-lg"
+          >
+            {OPTIONS.map(({ title, menus }, index) => {
               return (
-                <ListboxItem
-                  key={index}
-                  color="primary"
-                  className="px-3 hover:!rounded-small dark:!text-slate-100"
-                  onClick={() => router.push(href)}
-                  showDivider={showLine}
-                  startContent={<Icon icon={icon ?? ""} />}
-                >
-                  {label}
-                </ListboxItem>
+                <ListboxSection key={title} title={title}>
+                  {menus.map(
+                    ({ href, label, showLine = false, icon }, index) => (
+                      <ListboxItem
+                        key={index}
+                        color="primary"
+                        className="px-3 hover:!rounded-small dark:!text-slate-100"
+                        onClick={() => router.push(href)}
+                        showDivider={showLine}
+                        startContent={<Icon icon={icon ?? ""} />}
+                      >
+                        {label}
+                      </ListboxItem>
+                    ),
+                  )}
+                </ListboxSection>
               );
             })}
           </Listbox>

a6c7b18dd577abeafbabefd40686bde3fe0c3c1a
Joel Viloria <viloriajoel07@github.com>
2024-07-01 08:17:42 -0500

    animation accordion item

--- src/modules/animations.ts
+++ src/modules/animations.ts
@@ -42,3 +42,40 @@ export const onlyScale = {
     duration: 0.4,
   },
 };
+
+export const accordionItemAnimate = {
+  variants: {
+    enter: {
+      y: 0,
+      opacity: 1,
+      height: "auto",
+      transition: {
+        height: {
+          type: "spring",
+          stiffness: 500,
+          damping: 30,
+          duration: 1,
+        },
+        opacity: {
+          easings: "ease",
+          duration: 1,
+        },
+      },
+    },
+    exit: {
+      y: -10,
+      opacity: 0,
+      height: 0,
+      transition: {
+        height: {
+          easings: "ease",
+          duration: 0.25,
+        },
+        opacity: {
+          easings: "ease",
+          duration: 0.3,
+        },
+      },
+    },
+  },
+};

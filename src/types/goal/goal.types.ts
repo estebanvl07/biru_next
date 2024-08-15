3ff4308a761b610135473803d3f484fdc39be7b1
estebanvl07 <estebanvl07@github.com>
2024-08-15 09:27:55 -0500

    FIX: importing entities

--- src/types/goal/goal.types.ts
+++ src/types/goal/goal.types.ts
@@ -1,8 +1,7 @@
-import { Goals, Entity } from "@prisma/client";
+import { Goals, Entities } from "@prisma/client";
 import { TransactionIncludes } from "~/server/api/services/transactions.services";
 
 export interface GoalsIncludes extends Goals {
-    transactions: TransactionIncludes[];
-    entity: Entity
-  }
-  
+  transactions: TransactionIncludes[];
+  entity: Entities;
+}

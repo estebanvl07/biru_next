ac013623b391ec0fb88a4a3461a0c4c98a9f9f32
esteban_vl <viloriaalgarin@gmail.com>
2025-04-07 15:36:21 -0500

    FEAT: mejorar el layout de configuración, agregar navegación activa y optimizar la interfaz de usuario

--- src/modules/Layouts/SettingsLayout.tsx
+++ src/modules/Layouts/SettingsLayout.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import OverviewLayout from "./templates/dashbaord/OverviewLayout";
 import Link from "next/link";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
 
 const routes = [
   {
@@ -34,27 +36,49 @@ const routes = [
 ];
 
 const SettingsLayout = ({ children }: { children: React.ReactNode }) => {
+  const activeRef = useRef<HTMLAnchorElement | null>(null);
+
+  useEffect(() => {
+    if (activeRef.current) {
+      activeRef.current.scrollIntoView({
+        behavior: "smooth",
+        inline: "center",
+        block: "nearest",
+      });
+    }
+  }, []);
+  const pathname = usePathname();
+
   return (
-    <OverviewLayout>
-      <div className="border-b py-10">
-        <h2 className="text-3xl -tracking-wide">Configuración</h2>
-        <p>Lorem ipsum dolor sit amet.</p>
+    <OverviewLayout title="Configuración">
+      <div className="hidden border-b py-4 pb-8 md:block">
+        <h2 className="text-4xl -tracking-wide">Configuración</h2>
       </div>
-      <main className="mt-10 flex items-start gap-8">
-        <aside className="flex w-64 flex-col">
-          <ul className="flex flex-col">
-            {routes.map(({ href, name }) => (
-              <Link
-                key={name}
-                href={`/overview/settings/${href}`}
-                className="rounded-lg px-4 py-2 text-sm hover:bg-default-200"
-              >
-                {name}
-              </Link>
-            ))}
+      <main className="flex flex-col items-start gap-4 md:mt-10 md:flex-row md:gap-8">
+        <aside className="flex w-full flex-col rounded-xl md:w-64">
+          <ul className="flex  flex-row overflow-x-auto py-1 scrollbar-hide md:flex-col md:py-0">
+            {routes.map(({ href, name }) => {
+              const isActive = pathname.includes(href);
+              return (
+                <Link
+                  id={href}
+                  key={name}
+                  ref={isActive ? activeRef : null}
+                  href={`/overview/settings/${href}`}
+                  className={clsx(
+                    "whitespace-nowrap rounded-lg px-4 py-2 text-sm hover:bg-default-200",
+                    {
+                      "bg-default-200 font-semibold": isActive,
+                    },
+                  )}
+                >
+                  {name}
+                </Link>
+              );
+            })}
           </ul>
         </aside>
-        <div className="flex-grow">{children}</div>
+        <div className="w-full flex-grow md:w-auto">{children}</div>
       </main>
     </OverviewLayout>
   );
